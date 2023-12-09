93d6b61b09f684e767a5a6bbc1a97e53c4d6a48a
QN96 <141985079+QN96@users.noreply.github.com>
2023-12-09 18:58:33 +0700

    9/12

--- src/test/java/TH_Contacts/Actions/pageObject/ContactDetailPage.java
+++ src/test/java/TH_Contacts/Actions/pageObject/ContactDetailPage.java
@@ -7,29 +7,14 @@ import TH_Contacts.Interfaces.ContactDetailPageUI;
 
 public class ContactDetailPage {
     private static WebElement element = null;
-    private static BasePage basePage = new BasePage();
+    private static BasePage basePage=new BasePage();
 
-    public static String Detail(WebDriver driver) {
+    public static void Detail(WebDriver driver) {
         basePage.clickToElement(driver, ContactDetailPageUI.Contacts);
 
-        // Get the text and check if it matches the expected format
-        String totalContactsText = basePage.getElementText(driver, ContactDetailPageUI.Total_Contacts);
-        String[] parts = totalContactsText.split("/");
+        String totalClientsText = basePage.getElementText(driver, ContactDetailPageUI.Total_Contacts);
+        String lastPart = totalClientsText.split("/")[1].trim();
 
-        if (parts.length >= 2) {
-            // Extract the last part after the '/' and trim any leading/trailing whitespaces
-            String totalContactsCount = parts[1].trim();
-
-            System.out.println("Total Contacts: " + totalContactsCount);
-
-            // Return the totalContactsCount
-            return totalContactsCount;
-        } else {
-            System.out.println("Unexpected format: " + totalContactsText);
-            // You can add additional handling logic here, such as logging the error or throwing an exception
-
-            // Return a default or error value (null in this case)
-            return null;
-        }
+        System.out.println("Total Contact: " + lastPart);
     }
 }
